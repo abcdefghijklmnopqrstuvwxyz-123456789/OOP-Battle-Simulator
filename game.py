259fdf779b72798076ca4759b8719c5cf6dad3fb
C-Dog2010 <conor.nunamann@ascte.org>
2026-09-11 21:12:55 -0500

    Add second goblin entrance
    Add second goblin entrance

--- game.py
+++ game.py
@@ -11,8 +11,10 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Gribble")
+    goblin2 = Goblin("Grobble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
 
 
